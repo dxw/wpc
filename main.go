34fadfb77246ba365c88dac2e40ceafd789d1cc4
Mallory Adams <mallory@dxw.com>
2020-02-18 14:56:53 -0500

    Amend /vendor/ to the end of .gitignore, if it's not already present

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"text/template"
 
 	flag "github.com/spf13/pflag"
@@ -71,6 +72,8 @@ func main() {
 	creating("script/server", rawTemplates["templates/script/server.tmpl"], EXEC)
 	creating("script/console", rawTemplates["templates/script/console.tmpl"], EXEC)
 
+	amendGitIgnore([]string{"/vendor/"})
+
 	if *multisite {
 		makeDirs([]string{"config"})
 		creating("config/server.php", rawTemplates["templates/config/server.php.tmpl"], NOEXEC)
@@ -110,4 +113,45 @@ func okToWrite(file string) bool {
 		}
 	}
 	return true
+}
+
+func amendGitIgnore(expressions []string) {
+	byteContents, err := ioutil.ReadFile(".gitignore")
+	if err != nil {
+		byteContents = []byte{}
+	}
+	stringContents := string(byteContents)
+
+	lines := strings.Split(stringContents, "\n")
+
+	// If the last line is blank, ignore it
+	if lines[len(lines)-1] == "" {
+		lines = lines[0 : len(lines)-1]
+	}
+
+	fileChanged := false
+
+	for _, expression := range expressions {
+		expressionExists := false
+
+		for _, line := range lines {
+			if expression == line {
+				expressionExists = true
+				break
+			}
+		}
+
+		if !expressionExists {
+			lines = append(lines, expression)
+			fileChanged = true
+		}
+	}
+
+	// Add a blank line at the end - this will cause the file to end with a "\n"
+	lines = append(lines, "")
+
+	if fileChanged {
+		fmt.Println("-> Amending .gitignore")
+		ioutil.WriteFile(".gitignore", []byte(strings.Join(lines, "\n")), 0644)
+	}
 }
